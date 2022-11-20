88ddbf192914b67cb99e391570f0cb520f7a40e3
MinMangTongGye <minmangtonggye@gmail.com>
2022-11-20 21:31:29 +0900

    feature(Typo): H3, P 컴포넌트 작성

--- src/components/Typo.ts
+++ src/components/Typo.ts
@@ -2,17 +2,34 @@ import styled from '@emotion/styled';
 
 import {css} from "@emotion/react";
 
-const HEADER_STYLE = css`
+const NO_MARGIN = css`
   margin: 0; padding: 0;
 `;
 
 const H1 = styled.h1`
-  ${HEADER_STYLE};
+  ${NO_MARGIN};
   
   font-size: 36px;
+  color: #383838;
 `;
 
 
+const H3 = styled.h1`
+  ${NO_MARGIN};
+  
+  font-size: 16px;
+  color: #606060;
+`;
+
+const P = styled.p`
+  ${NO_MARGIN}; 
+  
+  font-size: 16px;
+  line-height: 20px;
+`;
+
 export default {
-  H1
+  H1,
+  H3,
+  P
 };
