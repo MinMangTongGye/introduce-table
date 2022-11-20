a8f534151ba68335a9aceccb4685422e1266858a
MinMangTongGye <minmangtonggye@gmail.com>
2022-11-20 21:31:29 +0900

    feature: Input 계열 컴포넌트 작성

---
+++ src/components/Input.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const Input = styled.input`
+  padding: 10px 12px;
+  
+  background: white;
+  
+  border-radius: 8px;
+  
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.16);
+  border: solid 1px rgba(168, 168, 168, 0.5);
+  
+  font-size: 14px;
+`;
+
+export const TextArea = styled.textarea`
+  padding: 10px 12px;
+  
+  background: white;
+  
+  border-radius: 8px;
+  
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.16);
+  border: solid 1px rgba(168, 168, 168, 0.5);
+  
+  font-size: 14px;
+`;
